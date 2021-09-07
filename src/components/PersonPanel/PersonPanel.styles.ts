079789a4a54379b61b56815deac7b3a52ab6074a
KotAtWork <RadioDeadInside>
2021-09-07 14:16:02 +0300

    feat: create Personal panel component with avatar

---
+++ src/components/PersonPanel/PersonPanel.styles.ts
@@ -0,0 +1,48 @@
+import { red } from '@material-ui/core/colors';
+import { Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: '120px',
+      width: '500px',
+      boxShadow: '0px 1px 4px rgba(0, 0, 0, .25)',
+      borderRadius: '4px',
+    },
+    cardContent: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '18px 22px',
+    },
+    blockIcon: {
+      fontSize: '48px',
+      opacity: '.7',
+      transition: 'all .2s ease-in-out',
+      '&:hover': {
+        cursor: 'pointer',
+        color: red.A700,
+        opacity: '1',
+        transform: 'scale(1.1)',
+      },
+      '&:active': {
+        cursor: 'pointer',
+        color: red.A100,
+        opacity: '.9',
+        transform: 'scale(.9)',
+      },
+    },
+    upperText: {
+      padding: '0 5px',
+      height: '14px',
+      fontWeight: theme.typography.fontWeightBold,
+    },
+    lowerText: {
+      padding: '0 5px',
+      height: '11px',
+    },
+  })
+);
+
+export { useStyles };
---
+++ src/defaultTypes.ts
@@ -0,0 +1,6 @@
+export interface IAvataraInfo {
+  firstName: string;
+  lastName: string;
+  size: 'large' | 'medium';
+  src?: string;
+}
