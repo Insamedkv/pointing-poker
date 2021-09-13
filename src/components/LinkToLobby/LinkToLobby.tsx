1f48ada32635629aac52cea18f55bdedb478e9b7
Koti4 <Koti4@DEV-ASUS>
2021-09-13 11:45:11 +0300

    feat: create linktolobby, sprint header and dealerpanel components

---
+++ src/components/LinkToLobby/LinkToLobby.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Container, Input, InputLabel } from '@material-ui/core';
+import { buttonTextConstants } from 'utils/buttonTextConstants';
+import { useStyles } from './LinkToLobby.sltyles';
+
+interface ILinkToLobbyProps {
+  link?: string;
+}
+
+const LinkToLobby: React.FC<ILinkToLobbyProps> = ({ link }) => {
+  const classes = useStyles();
+
+  const copyLink = () => {};
+
+  return (
+    <>
+      <InputLabel className={classes.inputLabel}>Link to lobby:</InputLabel>
+      <Container className={classes.container}>
+        <Input
+          type="text"
+          fullWidth
+          disableUnderline
+          readOnly
+          value={link || ''}
+          classes={{ root: classes.root, focused: classes.focused, input: classes.input }}
+        />
+        <Button color="primary" variant="contained" className={classes.inputButton}>
+          {buttonTextConstants.COPY}
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default LinkToLobby;
---
+++ src/components/SprintHeader/SprintHeader.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Container, IconButton, Typography } from '@material-ui/core';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import { IIssue } from 'defaultTypes';
+import { useStyles } from './SprintHeader.styles';
+
+interface ISprintHeaderProps {
+  sprintId: string;
+  issuesList: Array<IIssue>;
+}
+
+const SprintHeader: React.FC<ISprintHeaderProps> = ({ sprintId, issuesList }) => {
+  const classes = useStyles();
+  const baseTitle = `Sprint ${sprintId} planing, ISSUES: ${issuesList.map((issue) => issue.issueName).join(', ')}.`;
+  const [title, setTitle] = useState(baseTitle);
+
+  const startGame = () => {};
+  const cancelGame = () => {};
+
+  return (
+    <>
+      <Container className={classes.container}>
+        <Typography variant="h3" className={classes.root}>
+          {title}
+        </Typography>
+        <IconButton aria-label="edit" className={classes.editButton} size="small">
+          <EditOutlinedIcon className={classes.editButton} />
+        </IconButton>
+      </Container>
+    </>
+  );
+};
+
+export default SprintHeader;
