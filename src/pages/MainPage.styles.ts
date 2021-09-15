426c4e1216322d6a582ee19d70483f91304c741e
Cooler2012 <cool.er.af@gmail.com>
2021-09-16 00:05:42 +0300

    add mainPage

--- src/pages/MainPage.styles.ts
+++ src/pages/MainPage.styles.ts
@@ -1,9 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { SiteColors } from '../utils/styleConstants';
 
-export const useStyles = makeStyles(({ palette }) => ({
+export const useStyles = makeStyles(() => ({
   mainContainer: {
-    height: 'calc(100vh - 50px)',
+    height: 'calc(100vh - 120px)',
     maxWidth: 'none',
     width: '100%',
     display: 'flex',
@@ -15,7 +15,6 @@ export const useStyles = makeStyles(({ palette }) => ({
     width: '555px',
     height: '150px',
     borderRadius: '0px',
-    margin: '80px auto 10px',
   },
   title: {
     cursor: 'default',
